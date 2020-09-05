f652976bfb3ad1160b5e2f3dc453513a11cc713a
ilamkar <illum_dae@yahoo.com>
2020-09-05 11:31:24 -0500

    XMl COnversionn. Install XML install packege

--- OrgAPI/OrgAPI/Startup.cs
+++ OrgAPI/OrgAPI/Startup.cs
@@ -29,7 +29,9 @@ namespace OrgAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
             services.AddControllers(
+                // Exception Handling globally - custom Filter
                config=>config.Filters.Add(new MyExceptionFilter())
                );
             services.AddCors(options =>
